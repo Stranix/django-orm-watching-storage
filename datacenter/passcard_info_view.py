2635ae9a4a3289adbb248cbf55d9796a0b38ea95
Stranix <Phantom2525@gmail.com>
2023-03-22 15:46:27 +0300

    Refactor: passcard_info_view

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -7,21 +7,23 @@ from datacenter.models import format_duration
 
 
 def passcard_info_view(request, passcode):
-
     passcard = get_object_or_404(Passcard, passcode=passcode)
     all_passcard_visits = Visit.objects.filter(passcard=passcard)
-    this_passcard_visits = []
-
-    if all_passcard_visits:
-        for visit in all_passcard_visits:
-            this_passcard_visits.append({
-                'entered_at': visit.entered_at,
-                'duration': format_duration(visit.get_duration()),
-                'is_strange': visit.is_long()
-            })
+    formatted_passcard_visits = _get_format_visits_for_passcard_view(all_passcard_visits)
 
     context = {
         'passcard': passcard.owner_name,
-        'this_passcard_visits': this_passcard_visits
+        'this_passcard_visits': formatted_passcard_visits
     }
     return render(request, 'passcard_info.html', context)
+
+
+def _get_format_visits_for_passcard_view(visits: list[Visit]):
+    formatted_visits = []
+    for visit in visits:
+        formatted_visits.append({
+            'entered_at': visit.entered_at,
+            'duration': format_duration(visit.get_duration()),
+            'is_strange': visit.is_long()
+        })
+    return formatted_visits
